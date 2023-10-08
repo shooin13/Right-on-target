5ad155e9d68b4fcd9ecd5fae5f7477ea19c54aed
shooin13 <shooin13@gmail.com>
2023-10-08 19:06:25 +0300

    Added a calculation of 5 rounds total score and new game after 5 rounds

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
     
     //загаданное число
     var number: Int = Int.random(in: 1...50)
@@ -19,24 +20,75 @@ class ViewController: UIViewController {
     //сумма очков за раунд
     var points: Int = 0
     
+    private func newRound() {
+        number = Int.random(in: 1...50)
+        label.text = String(number)
+        round += 1
+    }
+    
+    private func newGame() {
+        newRound()
+        points = 0
+        round = 0
+        button.setTitle("Проверить", for: .normal)
+    }
+    
     
     
     @IBAction func checkNumber(_ sender: UIButton) {
-        let sliderNumber = Int(slider.value.rounded())
-        print("Попытка ввода \(sliderNumber)")
         
-        if sliderNumber == number {
-            print("Число угадано")
-            let alert = UIAlertController(title: "Игра окончена", message: "Вы угадали", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Играть еще раз", style: .default))
-            present(alert, animated: true)
-            number = Int.random(in: 1...50)
-            label.text = String(number)
+        if round < 5 {
+            print("раунд \(round)")
+            let sliderNumber = Int(slider.value.rounded())
+            print("Попытка ввода \(sliderNumber)")
+            
+            if sliderNumber == number {
+                let pointsEarned = 50
+                points += pointsEarned
+                print("Число угадано")
+                let alert = UIAlertController(title: "Вы угадали", message: "Вы заработали \(pointsEarned) баллов", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Продолжить", style: .default))
+                present(alert, animated: true)
+                
+                points += pointsEarned
+                
+                if round == 4 {
+                    button.setTitle("Показать результаты", for: .normal)
+                }
+                
+                newRound()
+            } else {
+                var pointsEarned = 0
+                
+                if sliderNumber > number {
+                    pointsEarned = 50 - (sliderNumber - number)
+                } else {
+                    pointsEarned = 50 - (number - sliderNumber)
+                }
+                print("промах на \(sliderNumber - number)")
+                let alert = UIAlertController(title: "Вы не угадали", message: "Число отличается на \(sliderNumber - number). Вы заработали \(pointsEarned) баллов", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Продолжить", style: .default))
+                present(alert, animated: true)
+                
+                points += pointsEarned
+                
+                if round == 4 {
+                    button.setTitle("Показать результаты", for: .normal)
+                }
+                
+                newRound()
+            }
+            
         } else {
-            print("промах на \(sliderNumber - number)")
-            let alert = UIAlertController(title: "Вы не угадали", message: "Число отличается на \(sliderNumber - number)", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Продолжить", style: .default))
+            
+            label.text = ""
+            let alert = UIAlertController(title: "Игра окончена", message: "По результатам 5 раундов вы заработали \(points) баллов. Вы молодец!", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Начать заново", style: .default))
             present(alert, animated: true)
+            
+            newGame()
         }
 
     }
